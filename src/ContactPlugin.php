21d47570dc36cf71cbbbb32b6943dc7a964d7724
Ryan Thompson <service@aiwebsystems.com>
2016-11-18 23:43:35 -0600

    Remove the plugin function

--- src/ContactPlugin.php
+++ src/ContactPlugin.php
@@ -12,32 +12,4 @@ use Anomaly\Streams\Platform\Addon\Plugin\Plugin;
 class ContactPlugin extends Plugin
 {
 
-    /**
-     * The plugin functions.
-     *
-     * @var ContactPluginFunctions
-     */
-    protected $functions;
-
-    /**
-     * Create a new ContactPlugin instance.
-     *
-     * @param ContactPluginFunctions $functions
-     */
-    public function __construct(ContactPluginFunctions $functions)
-    {
-        $this->functions = $functions;
-    }
-
-    /**
-     * Get the functions.
-     *
-     * @return array
-     */
-    public function getFunctions()
-    {
-        return [
-            new \Twig_SimpleFunction('contact_form', [$this->functions, 'form'], ['is_safe' => ['html']]),
-        ];
-    }
 }
